5aa7b3e455a98d5dd28785b8fb5024839b723bb0
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-03-24 18:39:55 -0500

    advances

---
+++ index.js
@@ -0,0 +1,63 @@
+const puppeteer = require('puppeteer')
+const landing = require('./src/components/ML/landing')
+
+
+
+// const init = async () => {
+
+
+    // try {
+        // const browser = await puppeteer.launch({ headless: false });
+        // const page = await browser.newPage();
+        // await page.setViewport({
+        //     height: 768,
+        //     width: 1366,
+        //     deviceScaleFactor: 1
+        // })
+        
+
+        // for (site of sites) {
+        //     await page.goto(site)
+        // }
+        // await page.goto('https://www.mercadolibre.com.co/inmuebles')
+        // await page.waitForSelector(`
+        //     .nav-search-classi >
+        //     .nav-search-classi-content >
+        //     .ch-popover-wrapper input`
+        // )
+        // await page.type(`
+        //     .nav-search-classi >
+        //     .nav-search-classi-content >
+        //     .ch-popover-wrapper input`,
+        //     ' Bogotá',
+        //     { delay: 100 }
+        // )
+        // await page.waitForSelector(`
+        //     .nav-search-classi >
+        //     .nav-search-classi-content >
+        //     .ch-popover-wrapper >
+        //     .ch-popover >
+        //     ul >
+        //     .ch-autocomplete-item`
+        // )
+        // await page.keyboard.press(`ArrowDown`, { delay: 300 })
+        // await page.keyboard.press(`ArrowDown`, { delay: 300 })
+        // await page.waitForSelector(`
+        //     .nav-search-classi >
+        //     .nav-search-classi-content >
+        //     .ch-popover-wrapper >
+        //     .ch-popover >
+        //     ul >
+        //     .ch-autocomplete-highlighted`
+        // )
+        // await page.keyboard.press(`Enter`, {delay: 400})
+    // } catch (error) {
+    //     console.log(`Something was wrong. ${error}`);
+    // }
+
+// }
+
+
+// init()
+
+
---
+++ info.js
@@ -0,0 +1,11 @@
+exports.sites = [
+    'https://listado.mercadolibre.com.co/inmuebles/casas/venta/bogota-dc/#origin=search&as_word=true',
+    'https://listado.mercadolibre.com.co/inmuebles/casas/arriendo/bogota-dc/#origin=search&as_word=true',
+    'https://listado.mercadolibre.com.co/inmuebles/apartamentos/venta/bogota-dc/#origin=search&as_word=true',
+    'https://listado.mercadolibre.com.co/inmuebles/apartamentos/arriendo/bogota-dc/#origin=search&as_word=true'
+]
+
+exports.searchOperations = [
+    
+]
+
