d9468d766ddedc375fa8232ea6a31534b1ee13a7
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-03-25 02:07:20 -0500

    advances to navigation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer')
+const $ = require('cheerio');
+
 const sites = [
     'https://listado.mercadolibre.com.co/inmuebles/casas/venta/bogota-dc/#origin=search&as_word=true',
     'https://listado.mercadolibre.com.co/inmuebles/casas/arriendo/bogota-dc/#origin=search&as_word=true',
@@ -18,13 +20,47 @@ const init = async () => {
             width: 1366,
             deviceScaleFactor: 1
         })
-        for (site of sites) {
-            await page.goto(site)
-            await page.screenshot({
-                path: `page-${sites.indexOf(site)}.jpg`,
-                fullPage: true,
-            })
+
+        await page.goto('https://listado.mercadolibre.com.co/inmuebles/casas/venta/bogota-dc/#origin=search&as_word=true')
+
+        const linkPages = await page.evaluate(() => {
+            const elements = document.querySelectorAll('ol > li .ui-search-result__wrapper .ui-search-result__image a')
+
+            const links = []
+            for (const element of elements) {
+                links.push(element.href)
+            }
+            return links
+        })
+
+        for (const linkPage of linkPages) {
+            await page.goto(linkPage)
+            console.log(`visitando la pagina ${linkPage}`);
         }
+
+
+
+
+        // const html = await page.evaluate(() => document.body.innerHTML)
+        // $('ol > li .ui-search-result__wrapper .ui-search-result__image ')
+
+        // for (site of sites) {
+        //         await page.goto(site)
+        //         const html = await page.evaluate(() => document.body.innerHTML)
+        //         $('.ui-search-breadcrumb__title', html).each(function() {
+        //             let title = $(this).text()
+        //             console.log(title);
+        //         })
+        //         $('ol > li .ui-search-result__wrapper a .ui-search-result__content-wrapper .ui-search-item__group--price span .price-tag-fraction', html).each(function () {
+        //             let prices = $(this).text()
+        //             console.log(prices)
+        //         })
+            //     await page.screenshot({
+        //         path: `page-${sites.indexOf(site)}.jpg`,
+        //         fullPage: true,
+        //     })
+        // }
+
     } catch (error) {
         console.log(`Something was wrong. ${error}`);
     }
