75fdcc580140c9666886cea3a1154563cddeee18
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-03-26 15:09:36 -0500

    refactor images and add parking, details and ubication exact

--- index.js
+++ index.js
@@ -24,18 +24,17 @@ const init = async () => {
         // await page.goto('https://listado.mercadolibre.com.co/inmuebles/casas/venta/bogota-dc/#origin=search&as_word=true')
 
         // const linkPages = await page.evaluate(() => {
-        //     const elements = document.querySelectorAll('ol > li .ui-search-result__wrapper .ui-search-result__image a')
+            // const links = []
+            // const elements = document.querySelectorAll('ol > li .ui-search-result__wrapper .ui-search-result__image a')
+                // .forEach(element => links.push(element.href))
 
-        //     const links = []
-        //     for (const element of elements) {
-        //         links.push(element.href)
-        //     }
-        //     return links
+            // return links
+
         // })
 
-        // for (const linkPage of linkPages) {
-        //     await page.goto(linkPage)
-        //     console.log(`visitando la pagina ${linkPage}`);
+        // for (const link of linkPages) {
+        //     await page.goto(link)
+        //     console.log(`visitando la pagina ${link}`);
         // }
 
         await page.goto('https://casa.mercadolibre.com.co/MCO-612776051-vendo-casa-en-villa-del-doradobogota-mls-21-429-_JM#position=1&type=item&tracking_id=337c21fe-425e-4f35-9482-a3c06845e517')
@@ -45,20 +44,28 @@ const init = async () => {
             const category = document.querySelector('.vip-classified-info dl').innerText
             const rooms = parseInt(document.querySelector('#productInfo .item-attributes .align-room').innerText)
             const bathrooms = parseInt(document.querySelector('#productInfo .item-attributes .align-bathroom').innerText)
-            const location = document.querySelector('section > div.section-map-title > div > h3').innerText
+
+            const ubication = {
+                address: document.querySelector('section > div.section-map-title > div > h2').innerText,
+                location: document.querySelector('section > div.section-map-title > div > h3').innerText,
+            }
+
+            const parking = parseInt(document.querySelector('.specs-container.specs-layout-alternate > ul > li:nth-last-child(2) > span').innerText)
             const age = document.querySelector('.specs-container.specs-layout-alternate > ul > li:last-child > span').innerText
             const description = document.querySelector('#description-includes p').innerText
-            const elements = document.querySelectorAll('#gallery_dflt > div a')
 
             const images = []
+            document.querySelectorAll('#gallery_dflt > div a')
+                .forEach(element => images.push(element.href))
 
-            for (const element of elements) {
-                images.push(element['href'])
-            }
+            const details = []
+            document.querySelectorAll('.item-details__content.ui-view-more__content > ul > li')
+                .forEach((element) => details.push(element.innerText))
 
             const price = parseInt(document.querySelector('#productInfo .price-tag-fraction')
                 .innerText
                 .replaceAll('.',''))
+
             const area = document.querySelector('#productInfo .item-attributes .align-surface')
                 .innerText
                 .split(' ', 2)
@@ -71,8 +78,10 @@ const init = async () => {
                 area,
                 rooms,
                 bathrooms,
-                location,
+                parking,
                 age,
+                details,
+                ubication,
                 description,
                 images,
             }
@@ -83,25 +92,10 @@ const init = async () => {
         console.log(property);
 
 
-        // const html = await page.evaluate(() => document.body.innerHTML)
-        // $('ol > li .ui-search-result__wrapper .ui-search-result__image ')
-
-        // for (site of sites) {
-        //         await page.goto(site)
-        //         const html = await page.evaluate(() => document.body.innerHTML)
-        //         $('.ui-search-breadcrumb__title', html).each(function() {
-        //             let title = $(this).text()
-        //             console.log(title);
-        //         })
-        //         $('ol > li .ui-search-result__wrapper a .ui-search-result__content-wrapper .ui-search-item__group--price span .price-tag-fraction', html).each(function () {
-        //             let prices = $(this).text()
-        //             console.log(prices)
-        //         })
-            //     await page.screenshot({
+        ///     await page.screenshot({
         //         path: `page-${sites.indexOf(site)}.jpg`,
         //         fullPage: true,
         //     })
-        // }
 
     } catch (error) {
         console.log(`Something was wrong. ${error}`);
