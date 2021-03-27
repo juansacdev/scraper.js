81645ad555d6e13c0fd7c5719b22ebe161a97d41
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-03-27 05:24:00 -0500

    fix bugs to write data

---
+++ src/components/ML/demo.js
@@ -0,0 +1,204 @@
+const puppeteer = require('puppeteer')
+const json2csv = require('json2csv')
+const fs = require('fs')
+
+const init = async () => {
+
+    try {
+        console.log('Start to scrape')
+        console.time('End to scrape');
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.setViewport({
+            height: 768,
+            width: 1366,
+            deviceScaleFactor: 1
+        })
+
+        await page.goto('https://casa.mercadolibre.com.co/MCO-613461989-vendo-casa-niza-mls-21-995-rcc-_JM#position=5&type=item&tracking_id=e7d8d199-8de5-493f-9476-65b57579bde4')
+        const property = await page.evaluate(() => {
+            const mainTitle = document.querySelector('.item-title h1')
+                .innerText
+                .replaceAll(/(\r\n|\n|\r)/gm, "")
+                .trim()
+            const category = document.querySelector('.vip-classified-info dl')
+                .innerText
+                .replaceAll(/(\r\n|\n|\r)/gm, "")
+                .trim()
+            const rooms = document.querySelector('#productInfo .item-attributes .align-room')
+                .innerText
+                .replaceAll(/(\r\n|\n|\r)/gm, "")
+                .trim()
+            const bathrooms = document.querySelector('#productInfo .item-attributes .align-bathroom')
+                .innerText
+                .replaceAll(/(\r\n|\n|\r)/gm, "")
+                .trim()
+
+            const ubication = {
+                address: document.querySelector('section > div.section-map-title > div > h2')
+                    .innerText
+                    .replaceAll(/(\r\n|\n|\r)/gm, "")
+                    .trim(),
+                location: document.querySelector('section > div.section-map-title > div > h3')
+                    .innerText
+                    .replaceAll(/(\r\n|\n|\r)/gm, "")
+                    .trim(),
+            }
+
+            const areaLabels = [
+                'Superficie total',
+                'Área construida',
+                'Superficie de terreno',
+            ]
+
+            const labels = [
+                document.querySelector('.specs-container ul li:first-child strong')
+                    .innerText
+                    .replaceAll(/(\r\n|\n|\r)/gm, "")
+                    .trim(),
+                document.querySelector('.specs-container ul li:nth-child(2) strong')
+                    .innerText
+                    .replaceAll(/(\r\n|\n|\r)/gm, "")
+                    .trim(),
+                document.querySelector('.specs-container ul li:nth-child(3) strong')
+                    .innerText
+                    .replaceAll(/(\r\n|\n|\r)/gm, "")
+                    .trim(),
+            ]
+
+            const values = [
+                document.querySelector('.specs-container ul li:first-child span')
+                    .innerText
+                    .replaceAll(/(\r\n|\n|\r)/gm, "")
+                    .trim(),
+                document.querySelector('.specs-container ul li:nth-child(2) span')
+                    .innerText
+                    .replaceAll(/(\r\n|\n|\r)/gm, "")
+                    .trim(),
+                document.querySelector('.specs-container ul li:nth-child(3) span')
+                    .innerText
+                    .replaceAll(/(\r\n|\n|\r)/gm, "")
+                    .trim(),
+            ]
+
+            const area = {
+                total: '',
+                built: '',
+                groud: '',
+            }
+
+            if (labels.includes(areaLabels[0])) {
+
+                area.total = values[0]
+
+                if (labels.includes(areaLabels[1])) {
+
+                    area.built = values[1]
+
+                    if (labels.includes(areaLabels[2])) {
+
+                        area.groud = values[2]
+
+                    }
+
+                }
+
+            }
+
+            const lastChild = document.querySelector('.specs-container.specs-layout-alternate > ul > li:last-child > strong')
+                .innerText
+                .replaceAll(/(\r\n|\n|\r)/gm, "")
+                .trim()
+            let age
+            let parking
+            let adminAmount = ''
+
+            switch (lastChild) {
+                case "Antigüedad":
+                    age = document.querySelector('.specs-container.specs-layout-alternate > ul > li:last-child > span')
+                        .innerText
+                        .replaceAll(/(\r\n|\n|\r)/gm, "")
+                        .trim()
+                    parking = document.querySelector('.specs-container.specs-layout-alternate > ul > li:nth-last-child(2) > span')
+                        .innerText
+                        .concat(' parqueadero')
+                        .replaceAll(/(\r\n|\n|\r)/gm, "")
+                        .trim()
+                    break;
+                case "Valor administración":
+                    adminAmount = document.querySelector('.specs-container.specs-layout-alternate > ul > li:last-child > span')
+                        .innerText
+                        .replaceAll(/(\r\n|\n|\r)/gm, "")
+                        .trim()
+                    age = document.querySelector('.specs-container.specs-layout-alternate > ul > li:nth-last-child(2) > span')
+                        .innerText
+                        .replaceAll(/(\r\n|\n|\r)/gm, "")
+                        .trim()
+                    parking = document.querySelector('.specs-container.specs-layout-alternate > ul > li:nth-last-child(3) > span')
+                        .innerText
+                        .concat(' parqueadero')
+                        .replaceAll(/(\r\n|\n|\r)/gm, "")
+                        .trim()
+                    break;
+            }
+
+            let description = document.querySelector('#description-includes p') || ''
+            if (description){
+                description = document.querySelector('#description-includes p')
+                    .innerText
+                    .replaceAll(/(\r\n|\n|\r)/gm, "")
+                    .trim()
+            }
+
+            const images = []
+            document.querySelectorAll('#gallery_dflt > div a')
+                .forEach(element => images.push(element.href))
+
+            const details = []
+            document.querySelectorAll('.item-details__content.ui-view-more__content > ul > li')
+                .forEach((element) => details.push(element.innerText))
+
+            const price = parseInt(document.querySelector('#productInfo .price-tag-fraction')
+                .innerText
+                .replaceAll('.',''))
+
+            return {
+                category,
+                mainTitle,
+                price,
+                rooms,
+                bathrooms,
+                parking,
+                age,
+                adminAmount,
+                details,
+                ubication,
+                area,
+                description,
+                images,
+            }
+
+        })
+        property.url = page.url()
+        console.log(property);
+
+        const data =[]
+        data.push(property)
+
+        const csv = json2csv.parse(data, {
+            quote:'',
+            escapedQuote: '',
+            delimiter: '|',
+        })
+
+        fs.writeFileSync('./src/public/demo.csv', csv, { encoding:'utf-8' })
+
+        await browser.close()
+
+    } catch (error) {
+        console.log(`Something was wrong. ${error}`);
+    }
+    console.timeEnd('End to scrape');
+}
+
+init()
---
+++ src/components/ML/index.js
@@ -0,0 +1,232 @@
+const puppeteer = require('puppeteer')
+const json2csv = require('json2csv')
+const fs  = require('fs');
+
+
+const sites = [
+    'https://listado.mercadolibre.com.co/inmuebles/casas/venta/bogota-dc/#origin=search&as_word=true',
+    'https://listado.mercadolibre.com.co/inmuebles/casas/arriendo/bogota-dc/#origin=search&as_word=true',
+    'https://listado.mercadolibre.com.co/inmuebles/apartamentos/venta/bogota-dc/#origin=search&as_word=true',
+    'https://listado.mercadolibre.com.co/inmuebles/apartamentos/arriendo/bogota-dc/#origin=search&as_word=true'
+]
+
+const init = async () => {
+
+    console.time('End to scrape');
+    try {
+        console.log('Starting to scrape')
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.setViewport({
+            height: 768,
+            width: 1366,
+            deviceScaleFactor: 1
+        })
+
+        await page.goto('https://listado.mercadolibre.com.co/inmuebles/casas/venta/bogota-dc/#origin=search&as_word=true')
+
+        const linkPages = await page.evaluate(() => {
+            const links = []
+            document.querySelectorAll('ol > li .ui-search-result__wrapper .ui-search-result__image a')
+                .forEach(element => links.push(element.href))
+
+            return links
+
+        })
+
+        let counter = 0
+        const properties = []
+
+        for (const link of linkPages) {
+            await page.goto(link)
+            await page.waitForSelector('.item-title h1')
+            const property = await page.evaluate(() => {
+                const mainTitle = document.querySelector('.item-title h1')
+                    .innerText
+                    .replaceAll(/(\r\n|\n|\r)/gm, "")
+                    .trim()
+                const category = document.querySelector('.vip-classified-info dl')
+                    .innerText
+                    .replaceAll(/(\r\n|\n|\r)/gm, "")
+                    .trim()
+                const rooms = document.querySelector('#productInfo .item-attributes .align-room')
+                    .innerText
+                    .replaceAll(/(\r\n|\n|\r)/gm, "")
+                    .trim()
+                const bathrooms = document.querySelector('#productInfo .item-attributes .align-bathroom')
+                    .innerText
+                    .replaceAll(/(\r\n|\n|\r)/gm, "")
+                    .trim()
+
+                const ubication = {
+                    address: document.querySelector('section > div.section-map-title > div > h2')
+                        .innerText
+                        .replaceAll(/(\r\n|\n|\r)/gm, "")
+                        .trim(),
+                    location: document.querySelector('section > div.section-map-title > div > h3')
+                        .innerText
+                        .replaceAll(/(\r\n|\n|\r)/gm, "")
+                        .trim(),
+                }
+
+                const areaLabels = [
+                    'Superficie total',
+                    'Área construida',
+                    'Superficie de terreno',
+                ]
+
+                const labels = [
+                    document.querySelector('.specs-container ul li:first-child strong')
+                        .innerText
+                        .replaceAll(/(\r\n|\n|\r)/gm, "")
+                        .trim(),
+                    document.querySelector('.specs-container ul li:nth-child(2) strong')
+                        .innerText
+                        .replaceAll(/(\r\n|\n|\r)/gm, "")
+                        .trim(),
+                    document.querySelector('.specs-container ul li:nth-child(3) strong')
+                        .innerText
+                        .replaceAll(/(\r\n|\n|\r)/gm, "")
+                        .trim(),
+                ]
+
+                const values = [
+                    document.querySelector('.specs-container ul li:first-child span')
+                        .innerText
+                        .replaceAll(/(\r\n|\n|\r)/gm, "")
+                        .trim(),
+                    document.querySelector('.specs-container ul li:nth-child(2) span')
+                        .innerText
+                        .replaceAll(/(\r\n|\n|\r)/gm, "")
+                        .trim(),
+                    document.querySelector('.specs-container ul li:nth-child(3) span')
+                        .innerText
+                        .replaceAll(/(\r\n|\n|\r)/gm, "")
+                        .trim(),
+                ]
+
+                const area = {
+                    total: '',
+                    built: '',
+                    groud: '',
+                }
+
+                if (labels.includes(areaLabels[0])) {
+
+                    area.total = values[0]
+
+                    if (labels.includes(areaLabels[1])) {
+
+                        area.built = values[1]
+
+                        if (labels.includes(areaLabels[2])) {
+
+                            area.groud = values[2]
+
+                        }
+
+                    }
+
+                }
+                const lastChild = document.querySelector('.specs-container.specs-layout-alternate > ul > li:last-child > strong')
+                    .innerText
+                    .replaceAll(/(\r\n|\n|\r)/gm, "")
+                    .trim()
+
+                let age
+                let parking
+                let adminAmount = ''
+
+                switch (lastChild) {
+
+                    case "Antigüedad":
+                        age = document.querySelector('.specs-container.specs-layout-alternate > ul > li:last-child > span')
+                            .innerText
+                            .replaceAll(/(\r\n|\n|\r)/gm, "")
+                            .trim()
+                        parking = document.querySelector('.specs-container.specs-layout-alternate > ul > li:nth-last-child(2) > span')
+                            .innerText
+                            .concat(' parqueadero')
+                            .replaceAll(/(\r\n|\n|\r)/gm, "")
+                            .trim()
+                        break;
+
+                    case "Valor administración":
+                        adminAmount = document.querySelector('.specs-container.specs-layout-alternate > ul > li:last-child > span')
+                            .innerText
+                            .replaceAll(/(\r\n|\n|\r)/gm, "")
+                            .trim()
+                        age = document.querySelector('.specs-container.specs-layout-alternate > ul > li:nth-last-child(2) > span')
+                            .innerText
+                            .replaceAll(/(\r\n|\n|\r)/gm, "")
+                            .trim()
+                        parking = document.querySelector('.specs-container.specs-layout-alternate > ul > li:nth-last-child(3) > span')
+                            .innerText
+                            .concat(' parqueadero')
+                            .replaceAll(/(\r\n|\n|\r)/gm, "")
+                            .trim()
+                        break;
+                }
+
+                let description = document.querySelector('#description-includes p') || ''
+                if (description){
+                    description = document.querySelector('#description-includes p')
+                        .innerText
+                        .replaceAll(/(\r\n|\n|\r)/gm, "")
+                        .trim()
+                }
+
+                const images = []
+                document.querySelectorAll('#gallery_dflt > div a')
+                    .forEach(element => images.push(element.href))
+
+                const details = []
+                document.querySelectorAll('.item-details__content.ui-view-more__content > ul > li')
+                    .forEach((element) => details.push(element.innerText))
+
+                const price = parseInt(document.querySelector('#productInfo .price-tag-fraction')
+                    .innerText
+                    .replaceAll('.',''))
+
+                return {
+                    category,
+                    mainTitle,
+                    price,
+                    rooms,
+                    bathrooms,
+                    parking,
+                    age,
+                    adminAmount,
+                    details,
+                    ubication,
+                    area,
+                    description,
+                    images,
+                }
+
+            })
+
+            property.url = page.url()
+            properties.push(property)
+
+            console.log(property)
+            console.log(`Page ${++counter} of ${linkPages.length} pages. Rest ${linkPages.length - counter} pages.`)
+
+        }
+
+        const csv = json2csv.parse(properties, {
+            quote:'',
+            escapedQuote: '',
+            delimiter: '|',
+        })
+
+        fs.writeFileSync('./src/public/data.csv', csv, { encoding:'utf-8' })
+        await browser.close()
+
+    } catch (error) {
+        console.log(`Something was wrong. ${error}`);
+    }
+    console.timeEnd('End to scrape');
+}
+
+init()
